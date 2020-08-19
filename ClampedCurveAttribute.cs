6865698ee7854c935346c373f682d92799e32e5f
Asger Strandby <asger60@gmail.com>
2020-08-19 21:37:21 +0200

    Add files via upload

---
+++ ClampedCurveAttribute.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace AnimationCurveClamped
+{
+    public class ClampedCurveAttribute : PropertyAttribute
+    {
+        public float screenHeight = -1;
+        public Rect boundsRect;
+
+        public ClampedCurveAttribute()
+        {
+            boundsRect = new Rect(0, 0, 1, 1);
+        }
+
+        public ClampedCurveAttribute(float x, float y)
+        {
+            boundsRect = new Rect(0, 0, x, y);
+        }
+
+
+        public ClampedCurveAttribute(float x, float y, float screenHeight)
+        {
+            boundsRect = new Rect(0, 0, x, y);
+            this.screenHeight = screenHeight;
+        }
+
+        public ClampedCurveAttribute(float xMin, float yMin, float xMax, float yMax)
+        {
+            boundsRect = new Rect(xMin, yMin, xMax - xMin, yMax - yMin);
+        }
+
+        public ClampedCurveAttribute(float xMin, float yMin, float xMax, float yMax, float screenHeight)
+        {
+            boundsRect = new Rect(xMin, yMin, xMax - xMin, yMax - yMin);
+
+            this.screenHeight = screenHeight;
+        }
+    }
+}
---
+++ ClampedCurveDrawer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace AnimationCurveClamped
+{
+    [CustomPropertyDrawer(typeof(ClampedCurveAttribute))]
+    public class ClampedCurveDrawer : PropertyDrawer
+    {
+        private float _height = -1;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            ClampedCurveAttribute clampedCurve = attribute as ClampedCurveAttribute;
+
+            if (clampedCurve == null)
+            {
+                return;
+            }
+            if (property.propertyType == SerializedPropertyType.AnimationCurve)
+            {
+                if (clampedCurve.screenHeight > 0)
+                {
+                    position.height = clampedCurve.screenHeight;
+                    _height = clampedCurve.screenHeight;
+                }
+            
+
+                EditorGUI.CurveField(position, property, Color.red, clampedCurve.boundsRect);
+            }
+            else
+            {
+                EditorGUI.LabelField(position, label.text, "Use ClampedCurve with AnimationCurve.");
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return _height <= -1 ? base.GetPropertyHeight(property, label) : _height;
+        }
+    }
+}
